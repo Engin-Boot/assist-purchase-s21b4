0dbb2396d5dbf15ba790749b62a2aeb4568600ca
Ajinkya Ghatol <ajinkyas.ghatol@gmail.com>
2020-10-23 14:55:30 +0530

    update filter ui

--- Client GUI/customerDeviceFilter/filter/MainWindow.xaml.cs
+++ Client GUI/customerDeviceFilter/filter/MainWindow.xaml.cs
@@ -19,9 +19,6 @@ using DataModels;
 
 namespace filter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         class GFG : IComparer<int>
@@ -50,36 +47,18 @@ namespace filter
                 weightRanges1.Add(t);
             }
             
-
-
-
-
             DataModels.FilterDataModel filters = new DataModels.FilterDataModel();
-
-            InitializeComponent();
             List<string> m = new List<string>();
-
-            m = ServerConnection.GetFiltersOptions.getMeasurementFeatures();
             DataModels.DeviceModel[] UpdatedListOfDevices = ServerConnection.GetDevices.getAllDevices();
             DataModels.DeviceModel[] PassedToStack2 = UpdatedListOfDevices;
             DataModels.DeviceModel[] PassedToStack3 = UpdatedListOfDevices;
             DataModels.DeviceModel[] PassedToStack4 = UpdatedListOfDevices;
-
-
-
+            InitializeComponent();
+            m = ServerConnection.GetFiltersOptions.getMeasurementFeatures();
             addMeasuremntstoFilterStack();
-
-
-
-            
-
-            //DataModels.DeviceModel[] passedToStack2;
             AddDevicesToDeviceStackPanel(UpdatedListOfDevices);
-            
-
             void addMeasuremntstoFilterStack()
             {
-                
                 StackPanel innerStack;
                 innerStack = new StackPanel
                 {
@@ -127,77 +106,7 @@ namespace filter
             }
             void AddFilterStack2()
             {
-                //if(UpdatedListOfDevices.Length>0)
-                //{
-                //    //StackPanel innerStack = new StackPanel
-                //    //{ Orientation = Orientation.Vertical };
-
-                //    Grid DynamicGrid = new Grid();
-
-                //    DynamicGrid.HorizontalAlignment = HorizontalAlignment.Left;
-                //    DynamicGrid.VerticalAlignment = VerticalAlignment.Top;
-                //    DynamicGrid.ShowGridLines = true;
-                //    DynamicGrid.Background = new SolidColorBrush(Colors.LightSteelBlue);
-
-                //    ColumnDefinition gridCol1 = new ColumnDefinition();
-
-                //    ColumnDefinition gridCol2 = new ColumnDefinition();
-
-
-                //    DynamicGrid.ColumnDefinitions.Add(gridCol1);
-
-                //    DynamicGrid.ColumnDefinitions.Add(gridCol2);
-
-                //    RowDefinition gridRow1 = new RowDefinition();
-
-
-
-                //    RowDefinition gridRow2 = new RowDefinition();
-
-                //    DynamicGrid.RowDefinitions.Add(gridRow1);
-
-                //    DynamicGrid.RowDefinitions.Add(gridRow2);
-
-                //    TextBox tminLabel = new TextBox{Text = "Min:",};    Grid.SetRow(tminLabel, 0);Grid.SetColumn(tminLabel, 0);
-                //    TextBox tmaxLabel = new TextBox { Text = "Max:", }; Grid.SetRow(tminLabel, 1); Grid.SetColumn(tminLabel, 0);
-
-                //    TextBox tmin = new TextBox{Text = "0"};Grid.SetRow(tmin, 0);Grid.SetColumn(tmin, 1);tmin.TextChanged += new TextChangedEventHandler(min_Weight_changed);
-                //    TextBox tmax = new TextBox{Text = "1000"}; Grid.SetRow(tmax, 1);Grid.SetColumn(tmax, 1);tmax.TextChanged += new TextChangedEventHandler(max_Weight_changed);
-                //    minWeight = 0;
-                //    maxWeight = 1000;
-                //    DynamicGrid.Children.Add(tminLabel);
-                //    DynamicGrid.Children.Add(tmaxLabel);
-                //    DynamicGrid.Children.Add(tmin);
-                //    DynamicGrid.Children.Add(tmax);
-
-                //    TextBlock filter2Label = new TextBlock { Text="Select Weight"};
-                //    filter2Stack.Children.Add(filter2Label);
-                //    filter2Stack.Children.Add(DynamicGrid);
-                //}
-
-                //if (UpdatedListOfDevices.Length > 0)
-                //{
-                //    StackPanel innerStack;
-                //    innerStack = new StackPanel
-                //    {
-                //        Orientation = Orientation.Vertical
-                //    };
-
-                //    //Build the item list
-                //    List<float> items = new List<float>();
-                //    foreach (var d in UpdatedListOfDevices)
-                //    {
-                //        if(!items.Contains(d.weight))
-                //        items.Add(d.weight);
-
-                //    }
-                //    ComboBox resCB = new ComboBox();
-                //    //Populate the ComboBox from the item list
-                //    resCB.ItemsSource = items;
-                //    innerStack.Children.Add(resCB);
-                //    resCB.SelectionChanged += new SelectionChangedEventHandler(wight_Selected);
-                //    filter2Stack.Children.Add(innerStack);
-                //}
+                
                 if (UpdatedListOfDevices.Length > 0)
                 {
                     PassedToStack2 = UpdatedListOfDevices;
@@ -295,94 +204,10 @@ namespace filter
                     }
                 }
             }
-            //void wight_Selected(object sender, SelectionChangedEventArgs e)
-            //{
-            //    ComboBox chk = (sender as ComboBox);
-            //    filters.weight.Clear();
-            //    filters.resolution.Clear();
-            //    filters.batterycapacity.Clear();
-            //    filters.weight.Add(float.Parse(chk.SelectedItem.ToString()));
-
-
-            //    updateDeviceStack();
-            //    filter3Stack.Children.Clear();
-            //    filter4Stack.Children.Clear();
-            //    if (filters.measurements.Count > 0)
-            //    {
-            //        AddFilterStack3();
-
-            //    }
-            //}
-            //void min_Weight_changed(object sender, TextChangedEventArgs e)
-            //{
-
-            //    TextBox chk = (sender as TextBox);
-            //    if(chk.Text.Length>0)
-            //    {
-            //        minWeight = int.Parse(chk.Text);
-            //        Weight_changed();
-            //    }
-
-            //}
-            //void max_Weight_changed(object sender, TextChangedEventArgs e)
-            //{
-            //    TextBox chk = (sender as TextBox);
-            //    if (chk.Text.Length > 0)
-            //    {
-            //        maxWeight = int.Parse(chk.Text);
-            //        Weight_changed();
-            //    }
-            //}
-
-            //void Weight_changed()
-            //{
-            //    List<float> availableDevicesWeight = new List<float>();
-            //    foreach(var u in UpdatedListOfDevices)
-            //    {
-            //        if(!availableDevicesWeight.Contains(u.weight))
-            //        {
-            //            availableDevicesWeight.Add(u.weight);
-            //        }
-            //    }
-            //    List<float> filteredWeights = new List<float>();
-            //    foreach(var v in availableDevicesWeight)
-            //    {
-            //        if(v>=minWeight && v<=maxWeight)
-            //        {
-            //            filteredWeights.Add(v);
-            //        }
-            //    }
-
-            //    filters.weight = filteredWeights;
-            //    updateDeviceStack();
-            //    filter3Stack.Children.Clear();
-            //    filter4Stack.Children.Clear();
-            //}
+            
             void AddFilterStack3()
             {
-                //if(UpdatedListOfDevices.Length>0)
-                //{
-                //    StackPanel innerStack;
-                //    innerStack = new StackPanel
-                //    {
-                //        Orientation = Orientation.Vertical
-                //    };
-
-                //    //Build the item list
-                //    List<string> items = new List<string>();
-                //    foreach (var d in UpdatedListOfDevices)
-                //    {
-                //        if (!items.Contains(d.resolution))
-                //            items.Add(d.resolution);
-
-                //    }
-                //    ComboBox resCB = new ComboBox();
-                //    //Populate the ComboBox from the item list
-                //    resCB.ItemsSource = items;
-                //    innerStack.Children.Add(resCB);
-                //    resCB.SelectionChanged += new SelectionChangedEventHandler(resolution_selected);
-                //    filter3Stack.Children.Add(innerStack);
-                //}
+                
                 if (UpdatedListOfDevices.Length > 0)
                 {
                     PassedToStack3 = UpdatedListOfDevices;
@@ -442,48 +267,10 @@ namespace filter
                 }
 
             }
-            //void resolution_selected(object sender, SelectionChangedEventArgs e)
-            //{
-            //    ComboBox chk = (sender as ComboBox);
-            //    filters.resolution.Clear();
-            //    filters.batterycapacity.Clear();
-            //    filters.resolution.Add(chk.SelectedItem.ToString());
-
-
-            //    updateDeviceStack();
-
-            //    filter4Stack.Children.Clear();
-            //    if (filters.measurements.Count > 0)
-            //    {
-            //        AddFilterStack4();
-
-            //    }
-            //}
+            
             void AddFilterStack4()
             {
-                //if (UpdatedListOfDevices.Length > 0)
-                //{
-                //    StackPanel innerStack;
-                //    innerStack = new StackPanel
-                //    {
-                //        Orientation = Orientation.Vertical
-                //    };
-
-                //    //Build the item list
-                //    List<string> items = new List<string>();
-                //    foreach (var d in UpdatedListOfDevices)
-                //    {
-                //        if (!items.Contains(d.batterycapacity))
-                //            items.Add(d.batterycapacity);
-
-                //    }
-                //    ComboBox resCB = new ComboBox();
-                //    //Populate the ComboBox from the item list
-                //    resCB.ItemsSource = items;
-                //    innerStack.Children.Add(resCB);
-                //    resCB.SelectionChanged += new SelectionChangedEventHandler(battery_selected);
-                //    filter4Stack.Children.Add(innerStack);
-                //}
+                
                 if (UpdatedListOfDevices.Length > 0)
                 {
                     PassedToStack4 = UpdatedListOfDevices;
@@ -539,16 +326,7 @@ namespace filter
                 
 
             }
-            //void battery_selected(object sender, SelectionChangedEventArgs e)
-            //{
-            //    ComboBox chk = (sender as ComboBox);
-            //    filters.batterycapacity.Clear();
-            //    filters.batterycapacity.Add(chk.SelectedItem.ToString());
-
-
-            //    updateDeviceStack();
-
-            //}
+           
             void updateDeviceStack()
             {
                 DevicesStack.Children.Clear();
@@ -585,12 +363,5 @@ namespace filter
             }
         }
 
-       
-
-        
-
-        
-
-       
     }
 }
